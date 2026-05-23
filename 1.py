e0121bf4bc7203e9677f65543977d531798a7ba6
koval.alexandra <koval.sashka07@gmail.com>
2026-05-23 11:04:02 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,13 @@
-s = input().split()
-a = {}
-for i in s:
-    if i in a:
-        a[i] += 1
+text = input().split()
+count = {}
+
+for word in text:
+    if word in count:
+        count[word] += 1
     else:
-        a[i] = 1
-words = sorted(a.items(), key=lambda x: x[1], reverse=True)
-for i in words:
-    print(i[0])
+        count[word] = 1
+
+result = sorted(count.items(), key=lambda x: x[1], reverse=True)
+
+for word, num in result:
+    print(word)
